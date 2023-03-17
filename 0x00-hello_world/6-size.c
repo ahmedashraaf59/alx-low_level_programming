52e6ded9926326258255329d79924c0d008adb47
ahmedashraaf59 <hamed.ahmed85@gmail.com>
2023-03-16 19:46:20 -0700

    shell tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,17 @@
 int main(void)
 {
 
-	int i;
-	double d;
 	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
 
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of an double is: %lu. \n", (unsigned long)sizeof(d));
-	printf("The size of an char is: %lu. \n", (unsigned long)sizeof(c));
+	printf("Size of a char is: %lu. \n", (unsigned long)sizeof(c));
+	printf("Size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int is: %lu. \n", (unsigned long)sizeof(li));
+	printf("Size of a long long int is: %lu. \n", (unsigned long)sizeof(lli));
+	printf("Size of a float is: %lu, \n", (unsigned long)sizeof(f));
 
 	return (0);
 }
