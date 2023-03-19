bf139fb2d28e0c942d5e21a8e11e7b34ca76cf37
ahmedashraaf59 <hamed.ahmed85@gmail.com>
2023-03-18 20:51:26 -0700

    if else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: base 16
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n = 48;
+	int m = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 102)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
