67fc66392a6bf4412e936fcc9d5232fb21c40648
ahmedashraaf59 <hamed.ahmed85@gmail.com>
2023-03-18 20:19:39 -0700

    if else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: 0 to 10
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
