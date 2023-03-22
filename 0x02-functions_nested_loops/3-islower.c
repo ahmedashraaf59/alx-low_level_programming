d7a86a95d323d1cfdd6f78064943feddfc359ec0
ahmedashraaf59 <hamed.ahmed85@gmail.com>
2023-03-22 16:49:37 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_islower - function to prints lower case
+ *
+ * @c: Parameter to be printed
+ *
+ * Return: 1 if it is a lower case
+ *and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c)
+		_putchar('1');
+	else
+		_putchar('0');
+	_putchar('\n');
+}
