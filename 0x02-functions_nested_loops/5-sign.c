87b250b460e87faba88000d1a0b0401735b4db5c
ahmedashraaf59 <hamed.ahmed85@gmail.com>
2023-03-22 18:56:02 -0700

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,26 @@
 #include "main.h"
 /**
- * print_sign - fumction that prints signs
+ * print_sign - function that prints signs
  *
  * @n function parameter
+ *
  * Return: 1,0,-1
  */
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar ('+');
 	return (1);
-	if else (n = 0);
+	}
+	else if n = 0
+	{
 		_putchar ('0');
 	return (0);
-	if else
+	}
+	else if
+	{
 	_putchar ('-');
 	return (-1);
+	}
 }
