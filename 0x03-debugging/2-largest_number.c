8e88dd1b370b787c8d7c6492face17810a949396
ahmedashraaf59 <hamed.ahmed85@gmail.com>
2023-03-24 17:55:09 -0700

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,17 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else if (c > a && c > b)
+	if (a > b && a > c)
+	{
+	largest = a;
+	}
+	else if (b > a && b > c)
+	{
+	largest = b;
+	}
+	else
 	{
 	largest = c;
 	}
